5bc398030d33813cb9cffd2ba11e7f0a4a6815f5
Benedict <benedict.roedel@student.uni-tuebingen.de>
2025-11-26 16:12:44 +0100

    added possibility to analyze multiple files together

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class Level(str, Enum):
     low = "low"
     medium = "medium"
     high = "high"
+    unknown = "unknown"
 
 
 class Finding(BaseModel):
@@ -25,15 +26,15 @@ class Finding(BaseModel):
     remediation: List[str]
     severity: Level
     snippet_context: str
+    file_path: str
 
 
 class FileReview(BaseModel):
-    file_hash: str
+    file_hash: int
     file_path: str
     language: str
-    findings: List[Finding]
+    finding: Finding
     # static_results: StaticResults, # TODO: when implemented bandit and/or semgrep
-    timestamp: str
 
 
 class Review(BaseModel):
@@ -41,6 +42,11 @@ class Review(BaseModel):
     vulnerability: Level
     most_vulnerable_files: List[str]
     timestamp: str
+
+
+class LLMReview(BaseModel):
+    results: List[Finding]
+    vulnerability: Level
 
 
 OLLAMA_CONFIG = {
@@ -67,17 +73,20 @@ def built_system_prompt() -> dict:
                  "The code will only be run in a encapsulated, internet-less environment and should only output results in a privacy preserving manner. " \
                  "Vulnerabilities you should detect are only such which endanger the data privacy " \
                  "by revealing patient-specific information to the hub of the federated learning system or by accidental leakage due to debug prints, logs, etc. " \
-                 "Furthermore, you should recognize attempts to break out from the container or to disguise data. "\
-                 "You do not need to flag code which uses patient-specific data to generate an output, if the output does not reveal any patient information"\
+                 "Furthermore, you should recognize attempts to break out from the container or to disguise data. " \
+                 "You do not need to flag code which uses patient-specific data to generate an output, if the output does not reveal any patient information" \
                  "Given one or multiple code files and their metadata, " \
-                 "return a single JSON object with fields: "\
-                 "vulnerability_types (list): list the types of vulnerabilities you recognize, "\
-                 "likelihood (float between 0 and 1): how likely is it, that the problem you found is a risk, "\
-                 "confidence ('low'|'medium'|'high'): how confident are you that the problem is a risk, "\
-                 "rationale (1-3 bullets): explain why the problem could be a risk, "\
-                 "remediation (1-3 bullets): what could be improved to lower the risk, " \
-                 "snippet_context (one-line): what is the user trying to achieve with the code. "\
-                 "NEVER provide exploit payloads, shell commands to exploit, or secrets. "\
+                 "return a single JSON object with fields: " \
+                 "1) vulnerability ('low'|'medium'|'high'): the estimated vulnerability risk for all files combined," \
+                 "2) results (list): containing a list of jsons with the following fields, create one entry in results for each file:" \
+                 "a) vulnerability_types (list): list the types of vulnerabilities you recognize, " \
+                 "b) likelihood (float between 0 and 1): how likely is it, that the problem you found is a risk, " \
+                 "c) confidence ('low'|'medium'|'high'): how confident are you that the problem is a risk, " \
+                 "d) rationale (1-3 bullets): explain why the problem could be a risk, " \
+                 "e) remediation (1-3 bullets): what could be improved to lower the risk, " \
+                 "f) snippet_context (one-line): what is the user trying to achieve with the code, " \
+                 "g) file_path (string): the path of the respective file." \
+                 "NEVER provide exploit payloads, shell commands to exploit, or secrets. " \
                  "Only non-actionable, high-level remediation guidance is allowed. " \
                  "Output only valid JSON."
 
@@ -91,7 +100,6 @@ def built_user_prompt(metadata, snippet) -> dict:
 
 
 def parse_llm_to_json(llm_response: str) -> dict:
-    print(llm_response)
     json_start = llm_response.find("{")
     json_end = llm_response.rfind("}")
 
@@ -102,7 +110,7 @@ def parse_llm_to_json(llm_response: str) -> dict:
         logger.warning("No JSON found in llm response (No '}' found)")
         return {}
 
-    json_string = llm_response[json_start:json_end+1]
+    json_string = llm_response[json_start:json_end + 1]
 
     try:
         json_response = json.loads(json_string)
@@ -114,37 +122,67 @@ def parse_llm_to_json(llm_response: str) -> dict:
         return {}
 
 
-def llm_json_to_findings(llm_json: dict) -> list:
-    findings = []
+def llm_json_to_findings(llm_json: dict) -> dict:
+    finding = {}
     if len(llm_json) == 0:
-        return []
+        return finding
 
     try:
-        vulnerability = llm_json.get("vulnerability_types", [])
-        likelihood = float(llm_json.get("likelihood", -1.0))
-        confidence = llm_json.get("confidence", "none")
-        rationale = llm_json.get("rationale", ["No rationale provided"])
-        remediation = llm_json.get("remediation", ["No remediation provided"])
-        severity = "high" if likelihood > 0.8 \
-            else "medium" if likelihood > 0.5 \
-            else "low" if likelihood >= 0.0 \
-            else "unknown"
-        snippet_context = llm_json.get("snippet_context", "No context provided")
-
-        findings.append({
-            "vulnerability_types": vulnerability,
-            "likelihood": likelihood,
-            "confidence": confidence,
-            "rationale": rationale,
-            "remediation": remediation,
-            "severity": severity,
-            "snippet_context": snippet_context
-        })
+        likelihood = float(llm_json.get("likelihood", 0.0))
+        severity = Level.high if likelihood > 0.8 \
+            else Level.medium if likelihood > 0.5 \
+            else Level.low if likelihood > 0.0 \
+            else Level.unknown
+        finding = {"vulnerability_types": llm_json.get("vulnerability_types", []), "likelihood": likelihood,
+                   "confidence": llm_json.get("confidence", Level.unknown),
+                   "rationale": llm_json.get("rationale", ["No rationale provided"]),
+                   "remediation": llm_json.get("remediation", ["No remediation provided"]),
+                   "snippet_context": llm_json.get("snippet_context", "No context provided"),
+                   "severity": severity,
+                   "file_path": llm_json.get("file_path", "No path provided")}
     except ValueError as e:
-        logger.warning("Exception while parsing llm json: %s\n"
+        logger.warning("Exception while parsing llm json to finding: %s\n"
                        "Probably caused by wrong json string from LLM", e)
 
-    return findings
+    return finding
+
+
+def llm_json_to_review(llm_json: dict, hashes: dict) -> dict:
+    review = {"results": [],
+              "most_vulnerable_files": []}
+
+    if len(llm_json) == 0:
+        logger.warning("JSON found in llm response has no data")
+        return {}
+
+    try:
+        review["vulnerability"] = llm_json.get("vulnerability", Level.unknown)
+        findings_list = llm_json.get("results", [])
+        for json_finding in findings_list:
+            finding = llm_json_to_findings(json_finding)
+            # validate json schema
+            jsonschema.validate(instance=finding, schema=Finding.model_json_schema())
+            file_path = finding.get("file_path")
+
+            file_review = {"finding": finding,
+                           "file_path": file_path,
+                           "file_hash": hashes.get(file_path, 0),
+                           "language": suffix_to_language(file_path)}
+
+            review["results"].append(file_review)
+
+            # add to list of most vulnerable files, if finding confidence is high
+            if finding.get("severity") == Level.high:
+                review["most_vulnerable_files"].append(file_path)
+
+        review["timestamp"] = datetime.now().isoformat()
+
+    except ValueError as e:
+        logger.warning("Exception while parsing llm json to review: %s\n"
+                       "Probably caused by wrong json string from LLM", e)#
+    except Exception as e:
+        logger.warning("Exception while validating finding: %s", e)
+    return review
 
 
 def main():
@@ -169,6 +207,7 @@ def main():
 
     # 2.2
     # 2.3 load file content and sanitize
+    hashes = {}
     for file_path in file_paths:
         file = open(file_path)
         file_content = file.read()  # TODO? Gesamte File mitgeben oder nur Ausschnitt?
@@ -179,36 +218,30 @@ def main():
         language = suffix_to_language(file_path)
         metadata = {"file_path": file_path, "language": language}
 
+        hashes[file_path] = sanitized_file_content.__hash__()
         llm_messages.append(built_user_prompt(metadata, sanitized_file_content))
-
+    llm_messages.append({"role": "user", "content": "Analyze all files now and output ONE single JSON"})
 
     response: ChatResponse = chat(model=OLLAMA_CONFIG.get("OLLAMA_MODEL"),
                                   messages=llm_messages,
-                                  format=Finding.model_json_schema(),
+                                  format=LLMReview.model_json_schema(),
                                   options={'temperature': OLLAMA_CONFIG.get("OLLAMA_TEMPERATURE")})
 
-    json_response = parse_llm_to_json(response.message.content)
-    findings = llm_json_to_findings(json_response)
-
     # 4. Format output to json
-    result = {
-        "file_hash": str(sanitized_file_content.__hash__()),
-        "file_path": file_path,
-        "language": language,
-        "findings": findings,
-        "timestamp": datetime.now().isoformat()
-    }
+    json_response = parse_llm_to_json(response.message.content)
+
+    review = llm_json_to_review(json_response, hashes)
 
     # validate json schema
     if jsonschema:
         try:
-            jsonschema.validate(instance=result, schema=FileReview.model_json_schema())
+            jsonschema.validate(instance=review, schema=Review.model_json_schema())
         except Exception as e:
-            logger.warning("Report validation failed: %s", e)
+            logger.warning("Review validation failed: %s", e)
 
     # 5. Output formatted json result
     print("output json:")
-    print(result)
+    print(review)
 
 
 if __name__ == '__main__':
